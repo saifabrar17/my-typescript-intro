95a912efe439921fc6d8e48d10064704f5329822
Saif Abrar <saifabrar.1998@gmail.com>
2023-04-05 11:26:54 +0600

    component switcher

--- src/components/componentSwitcher/ComponentSwitcher.tsx
+++ src/components/componentSwitcher/ComponentSwitcher.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import FirstComponent from "./FirstComponent";
 import SecondComponent from "./SecondComponent";
-// import classNames from 'classnames';
-
-type ComponentSwitcherProps = {
-  className?: string;
-};
+import ThirdComponent from "./ThirdComponent";
 
 enum ComponentType {
   FirstComponent,
   SecondComponent,
+  ThirdComponent,
 }
 
-const ComponentSwitcher: React.FC<ComponentSwitcherProps> = ({ className }) => {
+const ComponentSwitcher: React.FC = () => {
   const [currentComponent, setCurrentComponent] = useState(
     ComponentType.FirstComponent
   );
 
   const handleSwitchComponent = (componentType: ComponentType) => {
     setCurrentComponent(componentType);
+
+    console.log(componentType);
   };
 
   return (
@@ -46,6 +45,17 @@ const ComponentSwitcher: React.FC<ComponentSwitcherProps> = ({ className }) => {
         >
           Component 2
         </button>
+        <button
+          className={`py-2 px-4 rounded-md
+          ${
+            currentComponent === ComponentType.SecondComponent
+              ? "bg-blue-500 text-white"
+              : "bg-gray-300"
+          }`}
+          onClick={() => handleSwitchComponent(ComponentType.ThirdComponent)}
+        >
+          Component 3
+        </button>
       </div>
       <div className="mt-4">
         {currentComponent === ComponentType.FirstComponent && (
@@ -53,6 +63,9 @@ const ComponentSwitcher: React.FC<ComponentSwitcherProps> = ({ className }) => {
         )}
         {currentComponent === ComponentType.SecondComponent && (
           <SecondComponent />
+        )}
+        {currentComponent === ComponentType.ThirdComponent && (
+          <ThirdComponent />
         )}
       </div>
     </div>
---
+++ src/components/componentSwitcher/ThirdComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThirdComponent = () => {
+    return (
+        <div>
+            <p>this is third component</p>
+        </div>
+    );
+};
+
+export default ThirdComponent;
